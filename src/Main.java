d8b29b6eb8b32cdfefda2489991e965e003f5ed8
Solodovnikova Ekaterina <colo18@yandex.ru>
2018-04-12 15:18:11 +0500

    imported modules, dependencies and minor fixes at the main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import dispatcher.ThreadDispatcher;
+import worker.FileWorker;
+
 public class Main {
     public static void main(String[] args) {
-
+        FileWorker fw = new FileWorker("here is a path");
+        ThreadDispatcher td = new ThreadDispatcher();
     }
 }

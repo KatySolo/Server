b7a4d2c2a2bad076c47bfddaadff071dbff16801
Solodovnikova Ekaterina <colo18@yandex.ru>
2018-04-12 15:56:33 +0500

    improve server method

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,15 @@
 package main;
 
 import dispatcher.ThreadDispatcher;
+import server.WebServer;
 import worker.FileWorker;
 
 public class Main {
     public static void main(String[] args) {
-        FileWorker fw = new FileWorker("here is a path");
-        ThreadDispatcher td = new ThreadDispatcher();
+        WebServer server = new WebServer(333);
+        server.createSocket();
+        server.startServer();
+//        FileWorker fw = new FileWorker("here is a path");
+//        ThreadDispatcher td = new ThreadDispatcher();
     }
 }
--- src/server/WebServer.java
+++ src/server/WebServer.java
@@ -1,5 +1,7 @@
 package server;
 
+import client.Client;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -8,21 +10,38 @@ import java.net.Socket;
 
 public class WebServer {
     private int port;
+    private ServerSocket serverSocket;
 
     public WebServer(int serverPort) {
         port = serverPort;
     }
 
     public void createSocket () {
-        Socket client;
         try {
-            ServerSocket server = new ServerSocket(port);
-            client = server.accept();
-            InputStream in = client.getInputStream();
-            OutputStream out = client.getOutputStream();
+           serverSocket = new ServerSocket(port);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
+
+    public void startServer() {
+        Socket client;
+        try {
+            while (true) {
+               client = serverSocket.accept();
+                InputStream in = client.getInputStream();
+                OutputStream out = client.getOutputStream();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+//    Это основы, с которыми разобраться будет в общем случае несложно. Теперь несложно заметить, что построенный таким образом
+//        сервер является однопоточным - пока не будет вызван метод accept() - входящие соединения не будут приходить. Выходом из
+//        этой ситуации будет следующее:
+//        1. Поместить метод accept()  внутрь бесконечного цикла.
+//        2. Как только метод разблокируется, поместить клиентский сокет в другой поток, где будет обработан запрос, и благодаря
+//        пункту 1, сразу же перейти на новую итерацию цикла и снова вызвать метод accept()
